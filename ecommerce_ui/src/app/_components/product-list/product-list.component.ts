46a89d5aa06e7fca0a87da13f2efd4a47dd658ef
itpromeas <itpromeas@gmail.com>
2025-03-03 00:37:55 +0100

    product component setup

--- ecommerce_ui/src/app/_components/product-list/product-list.component.ts
+++ ecommerce_ui/src/app/_components/product-list/product-list.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgFor, NgbPaginationModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
+
+  products!: any[];
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.products = [
+      {
+        id: 1,
+        name: "product 1",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        unitPrice: 298
+      },
+      {
+        id: 2,
+        name: "product 1",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        unitPrice: 298
+      },
+      {
+        id: 3,
+        name: "product 1",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        unitPrice: 298
+      },
+      {
+        id: 4,
+        name: "product 1",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        unitPrice: 298
+      },
+      {
+        id: 5,
+        name: "product 1",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+        unitPrice: 298
+      }
+    ]
+  }
+
+  updatePageSize(pageSize: any) {
+    // todo
+  }
+
+  addToCart(val: any) {
+    
+  }
 
 }
--- ecommerce_ui/src/app/app.component.ts
+++ ecommerce_ui/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CategoryMenuComponent } from './_components/category-menu/category-menu.component';
+import { ProductListComponent } from './_components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CategoryMenuComponent, RouterLink],
+  imports: [RouterOutlet, CategoryMenuComponent, RouterLink, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
